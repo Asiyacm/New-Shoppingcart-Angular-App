6ecc2b33724fb8404d742879775bdb702d80fb53
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-08 01:51:11 +0530

    nav commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  fetchProducts=()=>{
+    return this.http.get("http://localhost:8080/viewall")
+  }
+  addProducts=(dataToSend:any)=>{
+    return this.http.post("http://localhost:8080/add",dataToSend)
+  }
+  searchProduct=(dataToSend:any)=>{
+     return this.http.post("http://localhost:8080/search",dataToSend)
+  }
+  addRegistration=(dataToSend:any)=>{
+     return this.http.post("http://localhost:8080/register",dataToSend)
+  }
+}
